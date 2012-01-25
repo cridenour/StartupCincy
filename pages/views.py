1d956319bde4cba06e77047d7a2ccae17cf432c4
Chris Ridenour <chrisridenour@gmail.com>
2012-01-24 22:21:29 -0500

    Add top three logos to homepage. Add media urls to urls.py

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
 
+from startups.models import Company
+
 class HomePage(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        companies = Company.objects.all()[0:3]
+
+        return {'companies': companies}
--- urls.py
+++ urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -17,4 +19,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)
+
+
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
