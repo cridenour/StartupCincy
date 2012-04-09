fd6953a4c304dc7b7b5e3dd6dc4910980218b4fb
Chris Ridenour <chrisridenour@gmail.com>
2012-04-09 02:59:32 -0400

    Remove not-null constraints

--- startup/startups/models.py
+++ startup/startups/models.py
@@ -8,14 +8,14 @@ from django.core.files.temp import NamedTemporaryFile
 class Company(models.Model):
     name = models.CharField(max_length=255)
     permalink = models.CharField(max_length=255, default='', blank=True, unique=True)
-    homepage = models.URLField(default='', blank=True)
-    blog = models.URLField(default='', blank=True)
+    homepage = models.URLField(default='', blank=True, null=True)
+    blog = models.URLField(default='', blank=True, null=True)
     twitter = models.CharField(max_length=255, default=None, blank=True, null=True)
     employees = models.IntegerField(default=None, blank=True, null=True)
-    founded = models.IntegerField(default=0, blank=True)
-    email = models.EmailField(default='', blank=True)
-    phone = models.CharField(max_length=20, default='', blank=True)
-    overview = models.TextField(default='', blank=True)
+    founded = models.IntegerField(default=0, blank=True, null=True)
+    email = models.EmailField(default='', blank=True, null=True)
+    phone = models.CharField(max_length=20, default='', blank=True, null=True)
+    overview = models.TextField(default='', blank=True, null=True)
     logo = models.ImageField(null=True, default=None, blank=True, upload_to='logo-cache')
 
     def crunchbase_url(self):
